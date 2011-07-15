b7c74f7309b19736da593e8cd07a359e20dae178
AJ Ribeiro <ajribeiro86@gmail.com>
2011-07-15 11:05:58 -0400

    stable

--- codebase/superdarn/src.bin/tk/tool/make_fitex2.1.0/make_fitex2.c
+++ codebase/superdarn/src.bin/tk/tool/make_fitex2.1.0/make_fitex2.c
@@ -2,7 +2,7 @@
    ==========
    Author: R.J.Barnes
 */
-"THIS IS A TEST THIS IS A TEST"
+
 /*
  (c) 2010 JHU/APL & Others - Please Consult LICENSE.superdarn-rst.3.2-beta-4-g32f7302.txt for more information.
 
