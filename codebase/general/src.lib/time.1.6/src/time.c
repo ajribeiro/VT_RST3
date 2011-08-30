1851002d5f73afa188a37b461249fe57b14cf6d3
AJ Ribeiro <ajribeiro86@gmail.com>
2011-08-30 16:02:40 -0400

    time.c is fixed for good

--- codebase/general/src.lib/time.1.6/src/time.c
+++ codebase/general/src.lib/time.1.6/src/time.c
@@ -4,40 +4,7 @@
 */
 
 /*
- Copyright 2004 The Johns Hopkins University/Applied Physics Laboratory.
- All rights reserved.
-
- This material may be used, modified, or reproduced by or for the U.S.
- Government pursuant to the license rights granted under the clauses at DFARS
- 252.227-7013/7014.
-
- For any other permissions, please contact the Space Department
- Program Office at JHU/APL.
-
- This Distribution and Disclaimer Statement must be included in all copies of
- "Radar Software Toolkit - SuperDARN Toolkit" (hereinafter "the Program").
-
- The Program was developed at The Johns Hopkins University/Applied Physics
- Laboratory (JHU/APL) which is the author thereof under the "work made for
- hire" provisions of the copyright law.
-
- JHU/APL assumes no obligation to provide support of any kind with regard to
- the Program.  This includes no obligation to provide assistance in using the
- Program or to provide updated versions of the Program.
-
- THE PROGRAM AND ITS DOCUMENTATION ARE PROVIDED AS IS AND WITHOUT ANY EXPRESS
- OR IMPLIED WARRANTIES WHATSOEVER.  ALL WARRANTIES INCLUDING, BUT NOT LIMITED
- TO, PERFORMANCE, MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE ARE
- HEREBY DISCLAIMED.  YOU ASSUME THE ENTIRE RISK AND LIABILITY OF USING THE
- PROGRAM TO INCLUDE USE IN COMPLIANCE WITH ANY THIRD PARTY RIGHTS.  YOU ARE
- ADVISED TO TEST THE PROGRAM THOROUGHLY BEFORE RELYING ON IT.  IN NO EVENT
- SHALL JHU/APL BE LIABLE FOR ANY DAMAGES WHATSOEVER, INCLUDING, WITHOUT
- LIMITATION, ANY LOST PROFITS, LOST SAVINGS OR OTHER INCIDENTAL OR
- CONSEQUENTIAL DAMAGES, ARISING OUT OF THE USE OR INABILITY TO USE THE
- PROGRAM."
-
-
-
+ (c) 2011 JHU/APL & Others - Please Consult LICENSE.superdarn-rst.3.3.txt for more information.
 
 
 
@@ -46,113 +13,127 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <string.h>
 #include <math.h>
 #include <time.h>
 #include "rtime.h"
 
-/*
- $Log: time.c,v $
- Revision 1.2  2004/04/26 21:16:32  barnes
- Audit - Enforced warning set to "all".
 
- Revision 1.1  2004/04/17 19:53:22  barnes
- Initial revision
+#define DAY_SEC 86400
 
-*/
+int TimeYMDHMSToYrsec(int yr,int mo,int dy,int hr,int mn,int sec) {
 
-#define DAY_SEC (24.0*3600.0)
-#define YEAR_SEC (365.0*24*3600.0)
-#define LYEAR_SEC (366.0*24.0*3600.0)
+  time_t clock;
+  struct tm tm;
+  char *tz;
 
-void TimeYrsecToYMDHMS(int time,int yr,
-              int *mo,int *dy,
-              int *hr,int *mn,int *sc) {
-  int yd;
-  int dt;
-  int n;
-  int *jday;
+  memset(&tm,0,sizeof(struct tm));
+  tm.tm_year=yr-1900;
+  tm.tm_mon=0;
+  tm.tm_mday=1;
+  tm.tm_hour=0;
+  tm.tm_min=0;
+  tm.tm_sec=0;
 
-  int nday[12] = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
-  int lday[12] = {0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335};
+  tz = getenv("TZ");
+  setenv("TZ", "", 1);
+  tzset();
+  clock=mktime(&tm);
 
-  if (((yr % 4)==0) && (((yr % 100) !=0) || ((yr % 400)==0))) jday=lday;
-  else jday=nday;
+  memset(&tm,0,sizeof(struct tm));
+  tm.tm_year=yr-1900;
+  tm.tm_mon=mo-1;
+  tm.tm_mday=dy;
+  tm.tm_hour=hr;
+  tm.tm_min=mn;
+  tm.tm_sec=sec;
+  clock=mktime(&tm)-clock;
 
-  yd=time/(24L*60L*60L); /* get the day of the year */
 
-  for (n=0;(n<12) && (yd>=jday[n]);n++);
+  if (tz) setenv("TZ", tz, 1);
+  else unsetenv("TZ");
+  tzset();
 
-  *mo=n; /* got the month */
-  if (n>0) *dy=1+yd-jday[n-1]; /* got the date */
-   else *dy=yd+1;
-
-  dt=time % (24L*60L*60L);
-  *hr=dt/(60L*60L);
-  *mn=(dt % (60L*60L))/60L;
-  *sc=dt % 60L;
+  return (int) clock;
 }
 
-int TimeYMDHMSToYrsec(int yr, int mo, int day, int hr, int mn,int sec) {
-  int t;
-  int jday[12] = {0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
-  int mday[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+void TimeYrsecToYMDHMS(int yrsec,int yr,int *mo,int *dy,int *hr,int *mn,
+	                int *sc) {
 
-  if ( (mo < 1) || (mo > 12) || (hr < 0) || (hr > 23) ||
-       (day <1) || (mn < 0) || (mn > 59) || (sec < 0) || (sec >= 60) )
-	   return -1;
 
-  if (day > mday[mo-1]) {
-	if (mo != 2) return -1;
-	else if (day != (mday[1] +1) || ((yr % 4) != 0)) return -1;
-  }
-  t = jday[mo-1] + day - 1;
+  time_t clock;
+  struct tm tmyr;
+  struct tm *tm;
+  char *tz;
 
-  if ( (mo > 2) && ((yr % 4) == 0) &&
-       (((yr % 100) !=0) || ((yr % 400)==0))) t = t+1;
+  memset(&tmyr,0,sizeof(struct tm));
+  tmyr.tm_year=yr-1900;
+  tmyr.tm_mon=0;
+  tmyr.tm_mday=1;
+  tmyr.tm_hour=0;
+  tmyr.tm_min=0;
+  tmyr.tm_sec=0;
 
-  return (((t*24L + hr)*60L + mn)*60L)+sec;
+  tz = getenv("TZ");
+  setenv("TZ", "", 1);
+  tzset();
+
+  clock=mktime(&tmyr);
+  if (tz) setenv("TZ", tz, 1);
+  else unsetenv("TZ");
+  tzset();
+
+  clock=clock+yrsec;
+  tm=gmtime(&clock);
+
+  *mo=tm->tm_mon+1;
+  *dy=tm->tm_mday;
+  *hr=tm->tm_hour;
+  *mn=tm->tm_min;
+  *sc=tm->tm_sec;
 }
+
 
 double TimeYMDHMSToEpoch(int yr,int mo,int dy,int hr,int mn,double sec) {
-   double tme;
-   int lpyear,ryear;
-   int yrsec;
-   if (yr<1970) return -1;
-   yrsec=TimeYMDHMSToYrsec(yr,mo,dy,hr,mn,(int) sec);
-   if (yrsec==-1) return -1;
-   tme=yrsec+(sec-(int) sec);
 
-   /* work out number of seconds from 1970 to start of year */
+  time_t clock;
+  struct tm tm;
+  char *tz;
 
-   lpyear=(yr-1969)/4;
-   ryear=(yr-1970)-lpyear;
+  memset(&tm,0,sizeof(struct tm));
+  tm.tm_year=yr-1900;
+  tm.tm_mon=mo-1;
+  tm.tm_mday=dy;
+  tm.tm_hour=hr;
+  tm.tm_min=mn;
+  tm.tm_sec=floor(sec);
 
-   tme+=(lpyear*LYEAR_SEC)+(ryear*YEAR_SEC);
-   return tme;
+  tz = getenv("TZ");
+  setenv("TZ", "", 1);
+  tzset();
+  clock=mktime(&tm);
+  if (tz) setenv("TZ", tz, 1);
+  else unsetenv("TZ");
+  tzset();
+
+  return clock+(sec-floor(sec));
 }
 
 void TimeEpochToYMDHMS(double tme,int *yr,int *mo,int *dy,int *hr,int *mn,
 	       double *sc) {
-  int yrsec=0;
-  int sec;
-  int i=0;
+  time_t clock;
+  struct tm *tm;
 
-  /* find the year */
+  clock=floor(tme);
+  tm=gmtime(&clock);
 
-  while ((yrsec<=tme) && (i<1e6)) {
-    if ((i % 4)==2) yrsec+=LYEAR_SEC;
-    else yrsec+=YEAR_SEC;
-    i++;
-  }
-  if (((i-1) % 4)==2) tme-=yrsec-LYEAR_SEC;
-  else tme-=yrsec-YEAR_SEC;
-  *yr=i+1969;
-  TimeYrsecToYMDHMS( (int) tme,*yr,mo,dy,hr,mn,&sec);
-
-  *sc=sec+(tme-(int) tme);
-
+  *yr=tm->tm_year+1900;
+  *mo=tm->tm_mon+1;
+  *dy=tm->tm_mday;
+  *hr=tm->tm_hour;
+  *mn=tm->tm_min;
+  *sc=tm->tm_sec+(tme-floor(tme));
 }
-
 
 double TimeYMDHMSToJulian(int yr,int mo,int dy,int hr,int mt,double sc) {
 
@@ -169,7 +150,7 @@ double TimeYMDHMSToJulian(int yr,int mo,int dy,int hr,int mt,double sc) {
   jdoy=i+1720994.5+B;
 
 
-  dfrac=1+TimeYMDHMSToYrsec(yr+1,mo,dy,hr,mt,sc)/DAY_SEC;
+  dfrac=1+(TimeYMDHMSToYrsec(yr+1,mo,dy,hr,mt,sc)+(sc-floor(sc)))/DAY_SEC;
 
   return jdoy+dfrac;
 
